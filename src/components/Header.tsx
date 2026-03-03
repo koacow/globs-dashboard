a998195f4f41614ecc0a1b52a0754ece524f5a82
koacow <khoa.cnd@gmail.com>
2026-03-03 14:27:46 -0500

    removed note in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,12 +17,6 @@ export default function Header() {
                 The Bilateral Relations Sentiment Index (BRSI) is a media-based measure of public sentiment, capturing the tone of local news coverage in Country 1 regarding Country 2.
                 The data is updated daily at 12:00 AM UTC.
             </Typography>
-            <Typography
-                variant="caption"
-                className="mb-8"
-            >
-                *The BRSI database is temporarily paused as we transition to a new database provider. Data updates will resume soon. The latest available data is current as of May 20th, 2025.
-            </Typography>
         </>
     )
 }
